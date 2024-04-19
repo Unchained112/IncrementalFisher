45fb05a3bad99590bbc8a7ae14434a3c13bfde63
Bosen Cheng <chengbosen112@gmail.com>
2024-04-19 14:34:13 +0200

    clean up

--- scripts/dev/game.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var bucket: TabBar = $CanvasLayer/HBoxContainer/TabContainer/Bucket
-
-
-
-
-func _on_button_button_down():
-	bucket.add_fish(DesignData.fish_list.pick_random())
